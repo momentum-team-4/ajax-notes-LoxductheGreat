e84102d11c9ad8d7fd4ef336471c994a8adf3da2
LoxductheGreat <jameelsalhhadin@gmail.com>
2020-08-31 15:06:35 -0400

    got things working

---
+++ newnice.js
@@ -0,0 +1,14 @@
+let text = document.querySelector('#notetext')
+let notelist = document.querySelector('#note-board')
+
+window.addEventListener('load', function(){
+
+})
+
+
+
+
+function  nicetext(){
+    let newtext = text.value
+    console.log()
+}
--- nice.js
+++ nice.js
@@ -1,50 +1,59 @@
+
+
+
 window.addEventListener('load', function(){
     fetch('http://localhost:3000/notes/')
     .then(response => response.json())
-    .then(data => {console.log(data)})
-                    
+    .then(data => {console.log(data)
+
+        for(i = 0; i < data.length; i++){
+            
+        let noteEl = document.createElement('div')
+        let textEl = document.createElement('p')
+        noteEl.classList.add('note')
+          textEl.innerHTML = data[i].body
+        finalnote.appendChild(noteEl)
+        noteEl.appendChild(textEl)
+        console.log(finalnote)
+        console.log(noteEl)}
+        })
 })
 
 
 // added elements
 let addnotebuttn = document.querySelector('#create-note')
 let addnotebar = document.querySelector('#addnotebar')
-// let notesDiv = document.querySelector('#notes')
 let finalnote = document.querySelector('#note-board')
-// let text = document.querySelector('')
- let text = document.querySelector('#notetext')
-
-// let displaytext = text.value
+ 
 
 
-addnotebuttn.addEventListener('click', function(){
-    let noteEl = document.createElement('div')
-    let textEl = document.createElement('p')
-    noteEl.classList.add('note')
 
-      textEl.innerHTML = text.value
-        
+addnotebuttn.addEventListener('click',
+    addnote )
 
-    // // console.log(addnotebuttn)
-    // let notebox = document.createElement('div')
-    // notebox.classList.add('sample-note')
-    // // console.log(notebox)  
-    // console.log(text)    
-    // finalnote.appendChild(notebox)
-    // console.log(finalnote)
 
-    finalnote.appendChild(noteEl)
-    noteEl.appendChild(textEl)
-    console.log(finalnote)
-    console.log(noteEl)
+function addnote(){
+    let text = document.querySelector('#notetext')
+    fetch('http://localhost:3000/notes/',{
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({"body": text.value})
 })
+        .then(response => response.json())
+        .then(data => {console.log(data)})
+    
+
+
+
+}
+
+
+
+
+    
 
 
 
 
 
-
-// make a feature to create notes 
-// store note after clicking create
-// store notes when clicking create button
-//  - 
+ 
